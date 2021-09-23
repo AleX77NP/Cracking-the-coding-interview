d45a1d63a3eee057f71ddbb96646908446da89c5
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2021-09-23 18:17:08 +0200

    binary tree

---
+++ TreesGraphs/TreesGraphs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TreesGraphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTreeNode root = new BinaryTreeNode(10);
+            root.Insert(12);
+            root.Insert(5);
+            root.Insert(56);
+            root.Insert(112);
+            root.Insert(167);
+            root.Display();
+
+            Console.WriteLine(IsBalanced(root));
+
+            
+        }
+
+        public static int MaxDepth(BinaryTreeNode t)
+        {
+            if(t == null)
+            {
+                return 0;
+            }
+
+            return 1 + Math.Max(MaxDepth(t._left), MaxDepth(t._right));
+        }
+
+        public static int MinDepth(BinaryTreeNode t)
+        {
+            if (t == null)
+            {
+                return 0;
+            }
+
+            return 1 + Math.Min(MaxDepth(t._left), MaxDepth(t._right));
+        }
+
+        public static bool IsBalanced(BinaryTreeNode t)
+        {
+            return (MaxDepth(t) - MinDepth(t) <= 1);
+        }
+    }
+}
---
+++ TreesGraphs/TreesGraphs/Tree.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreesGraphs
+{
+    public class BinaryTreeNode
+    {
+        private readonly int _value;
+        public BinaryTreeNode _left
+        {
+            private set;
+            get;
+        }
+        public BinaryTreeNode _right
+        {
+            private set;
+            get;
+        }
+
+        public BinaryTreeNode(int value)
+        {
+            _value = value;
+            _left = null;
+            _right = null;
+        }
+
+        public int Value()
+        {
+            return _value;
+        }
+
+        public BinaryTreeNode Parent
+        {
+            get;
+            private set;
+        }
+
+        public void Insert(int v)
+        {
+            if (v >= _value)
+            {
+                if(_right == null)
+                {
+                    AddChild(v);
+                } else
+                {
+                    _right.Insert(v);
+                }
+            } else
+            {
+                if (_left == null)
+                {
+                    AddChild(v);
+                } else
+                {
+                    _left.Insert(v);
+                }
+            }
+        }
+
+        private void AddChild(int v)
+        {
+            var child = new BinaryTreeNode(v) { Parent = this };
+            if (v > _value)
+            {
+                _right = child;
+            }
+            else
+            {
+                _left = child;
+            }
+        }
+
+        public void Display()
+        {
+            if (_left != null)
+            {
+                _left.Display();
+            }
+            Console.WriteLine(_value);
+            if (_right != null)
+            {
+                _right.Display();
+            }
+        }
+    }
+}
